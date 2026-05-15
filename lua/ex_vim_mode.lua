57a1f418b9c61e11bda534d23ff73ac810f7c66f
dynamicers <dynamicers@outlook.com>
2026-05-15 16:12:12 +0800

    lua: impore ex_vim_mode.lua

--- lua/ex_vim_mode.lua
+++ lua/ex_vim_mode.lua
@@ -1,8 +1,6 @@
 -- 参考 https://github.com/lei4519/blog/issues/85
 
--- 是否在中文模式的情况下被此程序切换为了英文模式
 local prev_is_cn_mode = false
--- 是否处于普通模式中 (ESC 触发)
 local in_normal_mode = false
 
 -- i, a, o, s, c, I, A, O, S, C
@@ -27,7 +25,6 @@ local function vim_mode(key, env)
 	end
 
 	local is_ascii_mode = env.engine.context:get_option("ascii_mode")
-	local keyCode = key.keycode
 
 	--- 1. 处理 Ctrl + o 进入 临时 Normal Mode 时切换---
 	--- 最佳实现是在回到 Insert Mode 时恢复原来的 ascii_mode 状态
@@ -40,7 +37,7 @@ local function vim_mode(key, env)
 	end
 
 	--- 2. 监听 ESC 键进入 Normal Mode ---
-	if key.keycode == 65307 then
+	if key.keycode == 65307 and in_normal_mode == false then
 		in_normal_mode = true
 		if is_ascii_mode then
 			prev_is_cn_mode = false
@@ -48,22 +45,25 @@ local function vim_mode(key, env)
 			prev_is_cn_mode = true
 			env.engine.context:set_option("ascii_mode", true)
 		end
+		return 2
 	end
 
-	--- 3. Normal Mode 重新进入 Insert Mode 时恢复 ---
-	if in_normal_mode then
-		if is_ascii_mode then
-			if enter_insert_keys[keyCode] then
-				in_normal_mode = false
-				if prev_is_cn_mode then
-					env.engine.context:set_option("ascii_mode", false)
-					return 1 -- 消费进入键，防止首字母直接上屏
-				end
-			end
-		end
-	end
+  -- 3. Normal Mode -> Insert Mode
+  if in_normal_mode and enter_insert_keys[key.keycode] then
+      in_normal_mode = false
+      -- 恢复原来的中文/英文模式
+      if prev_is_cn_mode then
+      	env.engine.context:clear()
+      	env.engine:commit_text(string.char(key.keycode))
+        env.engine.context:set_option("ascii_mode", false)
+        return 1 -- 消费首字母，防止首字母直接上屏
+      else
+        -- 英文模式直接允许输入
+        return 2
+      end
+  end
 
-	return 2
+  return 2
 end
 
 return vim_mode
